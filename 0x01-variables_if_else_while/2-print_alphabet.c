ab3250deb897111f4b5276fdbc3eb626d0e7fa36
collins860 <izuchukwuomeike76@gmail.com>
2022-12-10 08:42:14 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,38 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - prints all possible different combinations of two digits
+ * main - returns alphabets
  *
- *  Return: Always 0 (success)
- *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
+	int ch = 'a';
 
-	int ones = '0';
-	int tens = '0';
-
-	for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
+	while (ch <= 'z')
 	{
-		for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
-		{
-			if (!((ones == tens) || (tens > ones)))/*eliminates repitition*/
-			{
-				putchar(tens);
-				putchar(ones);
-				if (!(ones == '9' && tens == '8'))/*addes commma and space*/
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
+		putchar(ch);
+		ch++;
 	}
-
 	putchar('\n');
-
 	return (0);
 }
-
