034eef3c6aa22f14578f22df29de37a2214cc0bd
collins860 <izuchukwuomeike76@gmail.com>
2022-12-13 15:47:55 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,7 +4,6 @@
  * print_alphabet_x10 - print alpha 10 times
  *
  */
-
 void print_alphabet_x10(void)
 
 {
@@ -14,7 +13,6 @@ void print_alphabet_x10(void)
 	for (ten = 0; ten <= 9; ten++)
 	{
 		for (la = 'a'; la <= 'z'; la++)
-		{
 			_putchar(la);
 		_putchar('\n');
 	}
