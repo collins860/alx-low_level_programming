84589315d89a3a700b39e4a7787ee68bf981c053
collins860 <izuchukwuomeike76@gmail.com>
2022-12-13 18:41:52 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,10 +3,8 @@
 
 /**
  * print_times_table - prints time table with provided number
- * @n: the provided value by the user
- *
- * Return: Always 0 (Success)
- *
+ *  @n: the provided value by the user
+ *  Return: Always 0 (Success)
  */
 
 void print_times_table(int n)
